41326fd8004ee1dc9c033424a2d4fee5e98cee0b
dhruvesh9 <dun814@gmail.com>
2023-01-19 14:28:20 +0530

    bugfix - reading the json files

--- util/storage_util.js
+++ util/storage_util.js
@@ -1,18 +1,18 @@
 const fs = require('fs');
-
+const path = require('path');
 readDataFromStorage = function (isp, service) {
 
     if (isp == 'vodafone') {
         if (service == 'product_catalog') {
-            rawdata = fs.readFileSync('./storage/vodafone.product_catalog.json');
+            rawdata = fs.readFileSync(path.join(__dirname, '../storage/vodafone.product_catalog.json'));
         } else if (service == 'shopping_cart') {
-            rawdata = fs.readFileSync('./storage/vodafone.shopping_cart.json');
+            rawdata = fs.readFileSync(path.join(__dirname, '../storage/vodafone.shopping_cart.json'));
         }
     } else if (isp == 'orange') {
         if (service == 'product_catalog') {
-            rawdata = fs.readFileSync('./storage/orange.product_catalog.json');
+            rawdata = fs.readFileSync(path.join(__dirname, '../storage/orange.product_catalog.json'));
         } else if (service == 'shopping_cart') {
-            rawdata = fs.readFileSync('./storage/orange.shopping_cart.json');
+            rawdata = fs.readFileSync(path.join(__dirname, '../storage/orange.shopping_cart.json'));
         }
     }
 
@@ -25,15 +25,15 @@ writeDataIntoStorage = function (input_data, isp, service) {
     let data = JSON.stringify(input_data);
     if (isp == 'vodafone') {
         if (service == 'product_catalog') {
-            fs.writeFileSync('./storage/vodafone.product_catalog.json', data);
+            fs.writeFileSync(path.join(__dirname, '../storage/vodafone.product_catalog.json', data));
         } else if (service == 'shopping_cart') {
-            fs.writeFileSync('./storage/vodafone.shopping_cart.json', data);
+            fs.writeFileSync(path.join(__dirname, '../storage/vodafone.shopping_cart.json', data));
         }
     } else if (isp == 'orange') {
         if (service == 'product_catalog') {
-            fs.writeFileSync('./storage/orange.product_catalog.json', data);
+            fs.writeFileSync(path.join(__dirname, '../storage/orange.product_catalog.json', data));
         } else if (service == 'shopping_cart') {
-            fs.writeFileSync('./storage/orange.shopping_cart.json', data);
+            fs.writeFileSync(path.join(__dirname, '../storage/orange.shopping_cart.json', data));
         }
     }
 
