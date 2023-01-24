72b56d40ba961562e911bfd258d04e1676f734f5
dhruvesh9 <dun814@gmail.com>
2023-01-24 11:22:25 +0530

    changed the success response

--- util/response_util.js
+++ util/response_util.js
@@ -1,8 +1,6 @@
 function createResponse(error, dbResult) {
     var result = {};
     if (error == null || error == undefined) {
-        result['status'] = 'success';
-        result['code'] = 0;
         result['data'] = dbResult;
     } else {
         result['status'] = 'fail';
