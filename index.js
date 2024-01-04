5c95390800211247e5d970cbe00d083df55a1cff
Dhruvesh Navghare <dun814@gmail.com>
2024-01-04 12:44:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const authRoute = require('./routes/auth.route');
 
 const app = express();
 app.use((req, res, next) => {
+
+    // Logging request details
+    console.log('----------------------------------------');
+    console.log(`[${new Date().toLocaleString()}] Incoming Request:`);
+    console.log(`Method: ${req.method}`);
+    console.log(`URL: ${req.originalUrl}`);
+    console.log('Headers:', req.headers);
+    console.log('Query Parameters:', req.query);
+    console.log('Body:', req.body);
+    console.log('----------------------------------------');
+    
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
